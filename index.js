0783db06357705d4c4a17c4ec4e99a0106854761
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-02-27 11:34:35 +0530

    Step 2 - Add skeleton routes for user

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
  
+const express=require(express)
+
+const app=express();
+
+app.post('/user/signup',(req,res)=>{
+    res.json({
+        message:"sign up endpoint"
+    })
+})
+
+app.post('/user/sigin',(req,res)=>{
+    res.json({
+        message:"sign up endpoint"
+    })
+})
+
+app.get('/courses',(req,res)=>{
+    res.json({
+        message:"sign up endpoint"
+    })
+})
+
+app.post('/user/signup',(req,res)=>{
+    res.json({
+        message:"sign up endpoint"
+    })
+})
+app.get('/user/purchases',(req,res)=>{
+    // you would expect the user to pay you money
+    res.json({
+        message:"sign up endpoint"
+    })
+})
+
+app.post('/courses/purchases',(req,res)=>{
+    res.json({
+        message:"sign up endpoint"
+    })
+})
+
+
+
+
+
+app.listen(3000)
